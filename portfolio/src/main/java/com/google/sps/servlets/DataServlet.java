630e1530fa63902a2d30bcea7bc7f6c2c33a8db6
Eduardo <Herediae30@gmail.com>
2020-02-24 19:12:50 -0600

    Change the response when changing to /data URL.

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -21,12 +21,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
+//Tells the server which URL the servlet maps to.
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    //What the content that the client is expecting
     response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello world!</h1>");
+    //Responses
+    response.getWriter().println("<h1>Hello Ed!</h1>");
   }
 }
